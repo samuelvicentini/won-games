6fd8a44d6fb72d63912e472a954c2ca440e3a177
Samuel Vicentini <samuelvicentini@outlook.com>
2021-12-29 09:52:39 -0300

    create size test

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,4 +17,18 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
 })
